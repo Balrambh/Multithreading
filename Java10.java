f9aa50def27feb8d5d0f30abbe12819ac1fb4614
Balram <118844329+Balrambh@users.noreply.github.com>
2025-05-19 20:55:21 +0530

    Add files via upload

---
+++ Java10.java
@@ -0,0 +1,9 @@
+package com.mcq.multithreading_05_05;
+
+public class Java10 {
+	public static void main(String[] args) {
+		new Thread(()-> 
+		System.out.println("Naresh I Technologies")).start();
+    }
+}
+
